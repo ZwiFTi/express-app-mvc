04019e0a638ee0187a5ccc44df7a50f4315eca18
Andre Bomark <bomarkandre@gmail.com>
2018-07-01 06:24:23 +0200

    added: models & mongoose

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        required: [true, 'Let us know you by adding your name!'],
+        type: String
+    },
+    email: {
+        required: [true, 'Please add your email as well'],
+        type: String
+    }
+});
+
+const User = mongoose.model("User", UserSchema)
+module.exports = User
+
